6667cd5c6e51e5ecf4d14f6652f7bd599ef9c1be
chosunghyun18 <krismon@naver.com>
2024-02-12 14:52:35 +0900

    feat : add build

--- myDjango/setup.py
+++ myDjango/setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup, find_packages
 
+# requirements.txt에서 의존성 목록을 읽기
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
-    name="myDjango",  # 패키지 이름
-    version="0.1",  # 패키지 버전
-    packages=find_packages(),  # 패키지에 포함될 디렉토리 (자동으로 찾아줌)
-    include_package_data=True,  # 패키지 데이터 포함 여부
-    zip_safe=False,  # wheel을 압축 해제 없이 직접 실행 가능하게 할 것인지 여부
-    install_requires=[
-        "Django>=3.0",
-    ],
+    name="myDjango",
+    version="0.1.1",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=required,
 )
+
 
 
 # python setup.py sdist bdist_wheel
